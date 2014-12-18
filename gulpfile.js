a4185a2b298b79fe61c39c77e9a8ce8ed6d68e7b
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-18 11:01:31 +0900

    test with tape instead of mocha
    https://github.com/substack/tape

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
 'use strict';
 
+var spawn = require('child_process').spawn;
+
 var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
 var rimraf = require('rimraf');
 var stylish = require('jshint-stylish');
 
 gulp.task('lint', () => {
-  gulp.src('*.js')
+  return gulp.src('{,test/}*.js')
     .pipe($.jscs('package.json'))
     .pipe($.jshint('.jshintrc'))
     .pipe($.jshint.reporter(stylish))
     .pipe($.jshint.reporter('fail'));
-  gulp.src(['*.json'])
-    .pipe($.jsonlint())
-    .pipe($.jsonlint.reporter());
 });
 
 gulp.task('clean', cb => rimraf('test/actual', cb));
 
-gulp.task('test', ['clean', 'lint'], () => {
-  return gulp.src(['test/test.js'], {read: false})
-    .pipe($.mocha({reporter: 'spec'}));
+gulp.task('test', ['clean', 'lint'], cb => {
+  var cp = spawn('node', ['test/test.js'], {
+    stdio: [null, process.stdout, process.stderr]
+  });
+  cp.on('close', function(code) {
+    cb(code ? new Error('Exited with code ' + code) : null);
+  });
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['*.{js,json}'], ['test']);
+  gulp.watch(['{,test/}*.js', '*.json'], ['test']);
 });
 
 gulp.task('default', ['test', 'watch']);
--- test/test.js
+++ test/test.js
@@ -1,26 +1,27 @@
 'use strict';
 
-var fs = require('fs');
-var assert = require('assert');
 var spawn = require('child_process').spawn;
 
-describe('gulpur', () => {
-  it('should accept "gulpfile" option.', (done) => {
-    var gulpur = spawn('node', [
-      './index.js',
-      '--gulpfile',
-      'test/fixtures/gulpfile_custom_path.js'
-    ]);
-    
-    gulpur.on('close', () => {
-      fs.readFile('test/actual/result.txt', (err, data) => {
-        if (err) {
-          done(err);
-          return;
-        }
-        assert.strictEqual(data.toString(), 'foo,bar');
-        done();
-      });
+var fs = require('graceful-fs');
+var test = require('tape');
+
+test('gulpur', function(t) {
+  t.plan(2);
+
+  spawn('node', [
+    './index.js',
+    '--gulpfile',
+    'test/fixtures/gulpfile_custom_path.js'
+  ])
+  .on('close', function(code) {
+    fs.readFile('test/actual/result.txt', 'utf8', function(err, data) {
+      if (err) {
+        t.fail(err);
+        return;
+      }
+      t.equal(data, 'foo,bar', 'should accept "gulpfile" option.');
     });
-  });  
+
+    t.strictEqual(code, 0, 'should exit with code 0 if it closes without any errors.');
+  });
 });
